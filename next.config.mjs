41cf872e6790762580dbef46119e7ccdcff7879e
Maor <maorsa9@gmail.com>
2024-09-12 14:14:03 +0300

    Listing creation step 5 (Description and Price, Listing creation POST route)

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatars.githubusercontent.com", "lh3.googleusercontent.com"],
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
