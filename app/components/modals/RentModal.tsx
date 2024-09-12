64b199c1957949d575ad169b0b5398185d5faa9e
Maor <maorsa9@gmail.com>
2024-09-12 13:39:41 +0300

    Listing creation step 3 (Counter components)

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,7 +11,7 @@ import useRentModal from "@/app/hooks/useRentModal";
 
 import { categories } from "../navbar/Categories";
 import Modal from "./Modal";
-// import Counter from "../inputs/Counter";
+import Counter from "../inputs/Counter";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
 // import ImageUpload from "../inputs/ImageUpload";
@@ -174,36 +174,36 @@ const RentModal = () => {
     );
   }
 
-  // if (step === STEPS.INFO) {
-  //   bodyContent = (
-  //     <div className="flex flex-col gap-8">
-  //       <Heading
-  //         title="Share some basics about your place"
-  //         subtitle="What amenitis do you have?"
-  //       />
-  //       <Counter
-  //         onChange={(value) => setCustomValue("guestCount", value)}
-  //         value={guestCount}
-  //         title="Guests"
-  //         subtitle="How many guests do you allow?"
-  //       />
-  //       <hr />
-  //       <Counter
-  //         onChange={(value) => setCustomValue("roomCount", value)}
-  //         value={roomCount}
-  //         title="Rooms"
-  //         subtitle="How many rooms do you have?"
-  //       />
-  //       <hr />
-  //       <Counter
-  //         onChange={(value) => setCustomValue("bathroomCount", value)}
-  //         value={bathroomCount}
-  //         title="Bathrooms"
-  //         subtitle="How many bathrooms do you have?"
-  //       />
-  //     </div>
-  //   );
-  // }
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Share some basics about your place"
+          subtitle="What amenitis do you have?"
+        />
+        <Counter
+          onChange={(value) => setCustomValue("guestCount", value)}
+          value={guestCount}
+          title="Guests"
+          subtitle="How many guests do you allow?"
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue("roomCount", value)}
+          value={roomCount}
+          title="Rooms"
+          subtitle="How many rooms do you have?"
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue("bathroomCount", value)}
+          value={bathroomCount}
+          title="Bathrooms"
+          subtitle="How many bathrooms do you have?"
+        />
+      </div>
+    );
+  }
 
   // if (step === STEPS.IMAGES) {
   //   bodyContent = (
